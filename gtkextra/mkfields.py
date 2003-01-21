1ee586216de906e4a646648f7f5c4372bd12af7d
treeves <treeves>
2003-01-21 21:56:34 +0000

    more

--- gtkextra/mkfields.py
+++ gtkextra/mkfields.py
@@ -2,29 +2,27 @@
 
 # Copy C field definitions to this string.
 # Then run me.
+
 lines = """
+  gdouble rx1, rx2, ry1, ry2;
+  gint min_width, min_height;
 
-"""
+  GdkRectangle allocation;
 
+  guint state;
 
-"""
-  (fields
-   '("gboolean" "is_visible")
-   '("GtkPlotVector" "origin")
-   '("GtkPlotVector" "direction")
-   '("GtkPlotText" "title")
+  GtkPlotCanvasType type;
+  GtkPlotCanvasFlag flags;
+  GtkPlotCanvasSelection selection;
+  GtkPlotCanvasSelectionMode mode;
+
+  gpointer data;
+
 """
 
 import string
 
-print """  (fields"""
-for line in lines.split('\n'):
-    line = line.strip()
-    line = line.replace(';', '')
-    if not line:
-        continue
-
-    t,v = line.split(' ')
+def process_field(t,v):
     t = t.strip()
     v = v.strip()
     if v[0] == '*':
@@ -32,7 +30,28 @@ for line in lines.split('\n'):
         t += '*'
     
     print """    '("%s" "%s")""" % (t,v)
-print """  )"""
+
+def fields(lines):
+    print """  (fields"""
+    for line in lines.split('\n'):
+        line = line.strip()
+        line = line.replace(';', '')
+        if not line:
+            continue
+
+
+        x = line.split(',')
+        t,v = x[0].split(' ')
+        vlist = [v]+x[1:]
+
+        for v in vlist:
+            process_field(t,v)
+
+
+    print """  )"""
+
+
+fields(lines)
 
     
 
